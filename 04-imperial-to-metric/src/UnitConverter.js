979a61c5a70b0e4619fdeba6c469d0e71a1edf32
Andy Jakubowski <hello@andyjakubowski.com>
2020-04-24 08:49:07 +0200

    Start 04-imperial-to-metric prototype

---
+++ 04-imperial-to-metric/src/UnitConverter.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import UnitBox from './UnitBox';
+
+class UnitConverter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mg: 1000,
+    };
+
+    this.handleValueChange = this.handleValueChange.bind(this);
+  }
+
+  handleValueChange(event) {
+    const unit = event.target.name;
+    const value = event.target.value;
+
+    if (value === '') {
+      this.setState({
+        mg: '',
+      });
+
+      return;
+    }
+
+    switch (unit) {
+      case 'kg': {
+        this.setState({
+          mg: value * 1000000,
+        });
+        break;
+      }
+      case 'g': {
+        this.setState({
+          mg: value * 1000,
+        });
+        break;
+      }
+      case 'mg': {
+        this.setState({
+          mg: value,
+        });
+        break;
+      }
+      default: {
+        console.log('Can’t recognize this unit...');
+      }
+    }
+  }
+
+  render() {
+    const kg = this.state.mg / 1000000 || '';
+    const g = this.state.mg / 1000 || '';
+
+    return (
+      <>
+        <UnitBox unit="kg" value={kg} onValueChange={this.handleValueChange} />
+        <UnitBox unit="g" value={g} onValueChange={this.handleValueChange} />
+        <UnitBox unit="mg" value={this.state.mg} onValueChange={this.handleValueChange} />
+      </>
+    );
+  }
+}
+
+export default UnitConverter;
