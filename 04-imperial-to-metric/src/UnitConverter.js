37ad6cf4ffc7dfa5e90eb3b8467e35b44faa88ac
Andy Jakubowski <hello@andyjakubowski.com>
2020-04-24 11:22:08 +0200

    Generalize unit conversion so you can add more

--- 04-imperial-to-metric/src/UnitConverter.js
+++ 04-imperial-to-metric/src/UnitConverter.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import UnitBox from './UnitBox';
-import './UnitConverter.css';
+import React from "react";
+import UnitBox from "./UnitBox";
+import "./UnitConverter.css";
+
+const UNITS = ["mg", "g", "kg"];
+const RATIOS = {
+  mg: 1,
+  g: 1000,
+  kg: 1000000,
+};
 
 class UnitConverter extends React.Component {
   constructor(props) {
@@ -8,75 +15,56 @@ class UnitConverter extends React.Component {
 
     this.state = {
       mg: 1000,
-      mgInputValue: '1000',
+      activeUnit: "mg",
+      activeUnitValue: "1000",
     };
 
     this.handleValueChange = this.handleValueChange.bind(this);
   }
 
   handleValueChange(event) {
-    const unit = event.target.name;
-    let stringValue = event.target.value;
+    const activeUnit = event.target.name;
+    const activeUnitValue = event.target.value;
     let numberValue;
 
-    if (stringValue === '') {
-      this.setState({ mg: NaN, mgInputValue: stringValue });
+    if (activeUnitValue === "") {
+      this.setState({ mg: "NaN", activeUnit, activeUnitValue });
       return;
     }
 
-    numberValue = Number(stringValue);
+    numberValue = Number(activeUnitValue);
     if (Number.isNaN(numberValue)) {
-      this.setState({ mg: NaN, mgInputValue: stringValue });
+      this.setState({ mg: "NaN", activeUnit, activeUnitValue });
       return;
     }
 
-    switch (unit) {
-      case 'kg': {
-        const mg = numberValue * 1000000;
-        this.setState({
-          mg,
-          mgInputValue: String(mg),
-        });
-        break;
-      }
-      case 'g': {
-        const mg = numberValue * 1000;
-        this.setState({
-          mg,
-          mgInputValue: String(mg),
-        });
-        break;
-      }
-      case 'mg': {
-        this.setState({
-          mg: numberValue,
-          mgInputValue: stringValue,
-        });
-        break;
-      }
-      default: {
-        console.log('Can’t recognize this unit...');
-      }
-    }
+    this.setState({
+      mg: numberValue * RATIOS[activeUnit],
+      activeUnit,
+      activeUnitValue,
+    });
   }
 
   render() {
-    let g;
-    let kg;
-
-    if (Number.isNaN(this.state.mg)) {
-      g = '';
-      kg = '';
-    } else {
-      g = this.state.mg / 1000;
-      kg = this.state.mg / 1000000;
-    }    
-
     return (
       <div className="UnitConverter">
-        <UnitBox unit="mg" value={this.state.mgInputValue} onValueChange={this.handleValueChange} />
-        <UnitBox unit="g" value={g} onValueChange={this.handleValueChange} />
-        <UnitBox unit="kg" value={kg} onValueChange={this.handleValueChange} />
+        {UNITS.map((unit) => {
+          const value =
+            this.state.mg === "NaN" ? "" : this.state.mg * RATIOS[unit];
+
+          return (
+            <UnitBox
+              key={unit}
+              unit={unit}
+              value={
+                unit === this.state.activeUnit
+                  ? this.state.activeUnitValue
+                  : value
+              }
+              onValueChange={this.handleValueChange}
+            />
+          );
+        })}
       </div>
     );
   }
